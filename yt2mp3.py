97e21ea58f2d00fba9e5fab5be11a794c361e538
Joshua Cruz <60358264+polar1s7@users.noreply.github.com>
2020-12-09 04:12:08 +0800

    Update yt2mp3.py

--- yt2mp3.py
+++ yt2mp3.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import time
 import urllib3
 import os 
+from clint.textui import puts, colored
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -13,19 +14,21 @@ class Convert():
     def __init__(self, link):
         self.link = link
 
+
+
     def mp3(self):
         try:
             x = r.get('https://www.yt-download.org/api/widget/mp3/{}'.format(self.link))
             soup = BeautifulSoup(x.content, 'html.parser')
             url = r.get(soup.find_all('a')[0].get('href'))
             title = soup.find('h2').text
-            with open(f'{title}.mp3', "wb+") as f:
-                print('Downloading {}'.format(title))
-                f.write(url.content)
-            print('{} has been successfully downloaded.'.format(title))
+            open(f'{title}.mp3', "wb+").write(url.content)
+            print('Downloading..')
+            puts(colored.green('{} has been successfully downloaded.'.format(title)))
             time.sleep(5)
         except:
-            print('API might be broken or The link you input is wrong')
+            puts(colored.red('API might be broken or The link you input is wrong'))
+            time.sleep(5)
     
     def mp4(self):
         try:
@@ -33,19 +36,26 @@ class Convert():
             soup = BeautifulSoup(x.content, 'html.parser')
             url = r.get(soup.find_all('a')[0].get('href'))
             title = soup.find('h2').text
-            with open(f'{title}.mp4', "wb+") as f:
-                print('Downloading {}'.format(title))
-                f.write(url.content)
-            print('{} has been successfully downloaded.'.format(title))
+            open(f'{title}.mp4', "wb+").write(url.content)
+            puts(colored.green('{} has been successfully downloaded.'.format(title)))
             time.sleep(5)
         except:
-            print('API might be broken or The link you input is wrong')
+            puts(colored.red('API might be broken or The link you input is wrong'))
+            time.sleep(5)
+
 if __name__ == '__main__':
+    def clear():
+        os.system('cls' if os.name == 'nt' else 'clear')
     while True:
-        f = input('Format: e.g. mp4, mp3 : ')
-        link = input('Youtube Link : ')
+        print('1. MP4')
+        print('2. MP3')
+        f = input('> ')
+        clear()
+        print('Input the Youtube link e.g https://www.youtube.com/watch?v=i_kSqfHRXEo or i_kSqfHRXEo')
+        link = input('> ')
         c = Convert(link[-11:])
-        if 'mp4' in f:
+        if '1' in f:
             c.mp4()
         else:
             c.mp3()
+# Made better.
